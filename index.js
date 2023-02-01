f6a7714e5959840e89166a9d4a18849e2f8f0ce2
LR.btcmp <loretta.rehm@gmail.com>
2023-02-01 04:34:07 +0000

    Final version with generated example of html file

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const teamList = [];
 // Function to render the team
 function renderAndWriteTeam(team) { // Render the page
     const pageRender = render(team);
-    console.log(outputPath);
-    console.log(pageRender);
     // Write page on file
     fs.writeFile(outputPath, pageRender, (err) => err ? console.error(err) : console.log('Success!'));
 };
@@ -28,142 +26,28 @@ const init = async () => {
     let continueProgram = true;
     // Manager questions
     const managerQuestions = await questions.managerQuestions();
-    console.log(managerQuestions);
     const manager = new Manager(managerQuestions.managerName, managerQuestions.managerID, managerQuestions.managerEmail, managerQuestions.managerOfficeNumber);
     teamList.push(manager);
 
     // Check if user wants to keep adding employees
     while (continueProgram) {
         const menuQuestion = await questions.menuQuestion();
-        console.log("menuQuestion.action: " + menuQuestion.action);
 
         if (menuQuestion.action === "engineer") {
             const engineerQuestions = await questions.engineerQuestions();
-            console.log(engineerQuestions);
             const engineer = new Engineer(engineerQuestions.engineerName, engineerQuestions.engineerID, engineerQuestions.engineerEmail, engineerQuestions.engineerGithub);
             teamList.push(engineer);
         } else if (menuQuestion.action === "intern") {
             const internQuestions = await questions.internQuestions();
-            console.log(internQuestions);
+            const intern = new Intern(internQuestions.internName, internQuestions.internID, internQuestions.internEmail, internQuestions.internSchool);
+            teamList.push(intern);
         } else { // User wants to end the program
             continueProgram = false;
-            console.log('Exiting program.')
         }
     }
 
     // Render list once user finished adding employees
-    console.log(teamList);
     renderAndWriteTeam(teamList);
 };
 
 init();
-
-// ----------------------------------------------------------------
-// const menu = () => {
-//     console.log("MENU");
-//     const options = [{
-//             type: "list",
-//             name: "action",
-//             message: "What do you want to do?",
-//             choices: [
-//                 {
-//                     name: "Add an engineer",
-//                     value: "engineer"
-//                 }, {
-//                     name: "Add an intern",
-//                     value: "intern"
-//                 }, {
-//                     name: "Finish building the team",
-//                     value: "quit"
-//                 }
-//             ]
-//         }];
-//     return inquirer.prompt(options);
-// };
-
-// function main() {
-//     inquirer.prompt([
-//         {
-//             type: 'input',
-//             message: 'What is the name of the team manager?',
-//             name: 'managerName'
-//         }, {
-//             type: 'input',
-//             message: 'What is the employee ID of the team manager?',
-//             name: 'managerID'
-//         }, {
-//             type: 'input',
-//             message: 'What is the email of the team manager?',
-//             name: 'managerEmail'
-//         }, {
-//             type: 'input',
-//             message: 'What is the office number of the team manager?',
-//             name: 'managerOfficeNumber'
-//         },
-//     ]).then((response) => {
-//         const manager = new Manager(response.managerName, response.managerID, response.managerEmail);
-//         manager.setOfficeNumber(response.managerOfficeNumber);
-//         teamList.push(manager);
-//         programLoop();
-//     });
-// };
-
-// const programLoop = async () => {
-//     while (continueProgram) {
-//         await menu().then(option => {
-//             if (option.action === 'engineer') {
-//                 // return
-//                 const checkOK = new Promise((option, reject) => {addEngineer()});
-//                 console.log('Engineer added: '+ checkOK + " | continueProgram? " + continueProgram);
-//             } else if (option.action === 'inter') {
-//                 // const checkOK = new Promise((option, reject) => {addEngineer()});
-//                 console.log("continueProgram? " + continueProgram);
-//             } else {
-//                 console.log('Exiting program.')
-//                 console.log("continueProgram? " + continueProgram);
-//                 continueProgram = false;
-//                 console.log("continueProgram? " + continueProgram);
-//                 renderTeam(teamList);
-//                 // process.exit(0);
-//             }
-//         // }).then((data) => {
-//         //     console.log(util.inspect(data, {
-//         //         showHidden: false,
-//         //         depth: null
-//         //     }));
-//         }).catch((error, option) => {
-//             console.error('Error:', error);
-//         });
-//         console.log('End of while loop');
-//     }
-// };
-
-// function addEngineer() {
-//     inquirer.prompt([
-//         {
-//             type: 'input',
-//             message: 'What is the name of the engineer?',
-//             name: 'engineerName'
-//         }, {
-//             type: 'input',
-//             message: 'What is the employee ID of the engineer?',
-//             name: 'engineerID'
-//         }, {
-//             type: 'input',
-//             message: 'What is the email of the engineer?',
-//             name: 'engineerEmail'
-//         }, {
-//             type: 'input',
-//             message: 'What is the engineer\'s GitHub username?',
-//             name: 'engineerGithub'
-//         },
-//     ]).then((response) => {
-//         const engineer = new Engineer(response.engineerName, response.engineerID, response.engineerEmail, response.engineerGithub);
-//         teamList.push(engineer);
-//         console.log(engineer);
-//     });
-//     return true;
-// };
-
-
-// main();
--- src/page-template.js
+++ src/page-template.js
@@ -93,7 +93,7 @@ module.exports = team => {
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../src/style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
